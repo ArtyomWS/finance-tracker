0893ee9367b4ac92cfb492d47bf45699c8311526
ArtyomgdcGroup <artyom.serdyuk@gdcgroup.com>
2023-12-12 08:20:06 +0200

    add: transactions template

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,18 @@
       <USelectMenu v-model="selected" :options="transactionPeriods" placeholder="Select period" />
     </div>
   </section>
+  <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
+    <Trand color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false"/>
+    <Trand color="red" title="Income" :amount="2000" :last-amount="3000" :loading="false"/>
+    <Trand color="green" title="Income" :amount="1000" :last-amount="3000" :loading="false"/>
+    <Trand color="red" title="Income" :amount="3000" :last-amount="3000" :loading="true"/>
+  </section>
+  <section>
+    <Transactions />
+    <Transactions />
+    <Transactions />
+    <Transactions />
+  </section>
 </template>
 
 <script setup>
